8b76046ae9ab8fabe3ac5c44ba7a614715a3ee92
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-16 15:34:37 +0500

    Create Db context

---
+++ MascaradeApp.WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using MascaradeApp.WebAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MascaradeApp.WebAPI.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    public DbSet<Costume> Costumes { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Rental> Rentals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
