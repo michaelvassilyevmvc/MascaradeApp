032b2897bec89a195d9273df007c0ce1d057a0e4
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-16 16:21:15 +0500

    Expansion for processing errors in development or production

---
+++ MascaradeApp.WebAPI/StartupHelperExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace MascaradeApp.WebAPI;
+
+public static class StartupHelperExtensions
+{
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler(builder =>
+            {
+                builder.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    await context.Response.WriteAsync("An unexpected fault happended.Try again later.");
+                });
+            });
+        }
+        return app;
+    }
+}
