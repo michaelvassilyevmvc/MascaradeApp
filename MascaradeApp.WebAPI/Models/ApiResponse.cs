dfff20dd7e0dfdb8f27c6e5f680b37436bc3f754
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-16 16:20:12 +0500

    Model for forming a response

---
+++ MascaradeApp.WebAPI/Models/ApiResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace MascaradeApp.WebAPI.Models;
+
+public class ApiResponse
+{
+    public bool IsSuccess { get; set; }
+    public Object Result { get; set; }
+    public HttpStatusCode StatusCode { get; set; }
+    public List<string> ErrorMessages { get; set; }
+}
