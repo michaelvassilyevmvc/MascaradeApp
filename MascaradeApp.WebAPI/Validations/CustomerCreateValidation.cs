2054115ed03e591597248ca550faa370b7404acf
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-24 16:02:02 +0500

    Validation rules for Customer

---
+++ MascaradeApp.WebAPI/Validations/CustomerCreateValidation.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using MascaradeApp.WebAPI.DTO;
+
+namespace MascaradeApp.WebAPI.Validations;
+
+public class CustomerCreateValidation: AbstractValidator<CustomerCreateDto>
+{
+    public CustomerCreateValidation()
+    {
+        RuleFor(x => x.FullName)
+            .NotEmpty()
+            .WithMessage($"FullName имя требуется");
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty().WithMessage("Номер телефона обязателен.")
+            .Matches(@"^\+?[0-9]{7,15}$").WithMessage("Неверный формат номера телефона.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email обязателен.")
+            .EmailAddress().WithMessage("Неверный формат email.");
+    }
+}
---
+++ MascaradeApp.WebAPI/Validations/CustomerUpdateValidation.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using MascaradeApp.WebAPI.DTO;
+
+namespace MascaradeApp.WebAPI.Validations;
+
+public class CustomerUpdateValidation : AbstractValidator<CustomerUpdateDto>
+{
+    public CustomerUpdateValidation()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThan(0)
+            .WithMessage("Идентификатор клиента должен быть положительным.");
+
+        RuleFor(x => x.FullName)
+            .NotEmpty()
+            .WithMessage("Полное имя обязательно.")
+            .MaximumLength(100)
+            .WithMessage("Имя не должно превышать 100 символов.");
+
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty()
+            .WithMessage("Номер телефона обязателен.")
+            .Matches(@"^\+?[0-9]{7,15}$")
+            .WithMessage("Неверный формат номера телефона.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("Email обязателен.")
+            .EmailAddress()
+            .WithMessage("Неверный формат email.");
+
+        RuleFor(x => x.RegisteredAt)
+            .NotEmpty()
+            .WithMessage("Дата регистрации обязательна.")
+            .LessThanOrEqualTo(DateTime.UtcNow)
+            .WithMessage("Дата регистрации не может быть в будущем.");
+    }
+}
