006447d07a8670184d8dc6450885699e16873e2d
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-24 16:01:11 +0500

    Mapper for Customer

---
+++ MascaradeApp.WebAPI/Profiles/CustomerProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MascaradeApp.WebAPI.DTO;
+using MascaradeApp.WebAPI.Entities;
+
+namespace MascaradeApp.WebAPI.Profiles;
+
+public class CustomerProfile: Profile
+{
+    public CustomerProfile()
+    {
+        CreateMap<CustomerDto, Customer>().ReverseMap();
+        CreateMap<CustomerUpdateDto, Customer>().ReverseMap();
+        CreateMap<CustomerUpdateDto, CustomerDto>().ReverseMap();
+        CreateMap<CustomerCreateDto, Customer>().ReverseMap();
+        CreateMap<CustomerCreateDto, CustomerDto>().ReverseMap();
+    }
+}
