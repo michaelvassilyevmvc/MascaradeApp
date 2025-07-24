1895ae54ccd1d8d6acc8dc3b5717985e0c27722c
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-24 16:01:38 +0500

    Add Repository for Customer

---
+++ MascaradeApp.WebAPI/Services/CustomerRepository.cs
@@ -0,0 +1,46 @@
+﻿using MascaradeApp.WebAPI.Data;
+using MascaradeApp.WebAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MascaradeApp.WebAPI.Services;
+
+public class CustomerRepository : ICustomerRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public CustomerRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<ICollection<Customer>> GetAllCustomersAsync() =>
+        await _db.Customers.AsNoTracking()
+            .ToListAsync();
+
+    public async Task<Customer> GetAsync(int id) => await _db.Customers.AsNoTracking()
+        .FirstOrDefaultAsync(x => x.Id == id);
+
+    public async Task<Customer> GetAsync(string customerName) => await _db.Customers.AsNoTracking()
+        .FirstOrDefaultAsync(x => x.FullName.ToLower() == customerName.ToLower());
+
+    public async Task CreateAsync(Customer customer)
+    {
+        await _db.Customers.AddAsync(customer);
+    }
+
+    public async Task UpdateAsync(Customer customer)
+    {
+        _db.Update(customer);
+        await _db.SaveChangesAsync();
+    }
+
+    public void Delete(Customer customer)
+    {
+        _db.Customers.Remove(customer);
+    }
+
+    public async Task SaveAsync()
+    {
+        await _db.SaveChangesAsync();
+    }
+}
---
+++ MascaradeApp.WebAPI/Services/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using MascaradeApp.WebAPI.Entities;
+
+namespace MascaradeApp.WebAPI.Services;
+
+public interface ICustomerRepository
+{
+    Task<ICollection<Customer>> GetAllCustomersAsync();
+    Task<Customer> GetAsync(int id);
+    Task<Customer> GetAsync(string customerName);
+    Task CreateAsync(Customer customer);
+    Task UpdateAsync(Customer customer);
+    void Delete(Customer customer);
+    Task SaveAsync();
+}
