3e18155d03a0ffef240e3fcbc8942fd550618607
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-07-24 16:00:56 +0500

    Create Dtos for Customer

---
+++ MascaradeApp.WebAPI/DTO/CustomerCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using MascaradeApp.WebAPI.Entities;
+
+namespace MascaradeApp.WebAPI.DTO;
+
+public class CustomerCreateDto
+{
+    public string FullName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+
+    // public ICollection<Rental> Rentals { get; set; } = new List<Rental>();
+}
---
+++ MascaradeApp.WebAPI/DTO/CustomerDto.cs
@@ -0,0 +1,13 @@
+﻿using MascaradeApp.WebAPI.Entities;
+
+namespace MascaradeApp.WebAPI.DTO;
+
+public class CustomerDto
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime RegisteredAt { get; set; }
+
+}
